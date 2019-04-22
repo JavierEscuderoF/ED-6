812979f7b35326851ce3125c118cb771189aab96
Javier <javier.escudero90@gmail.com>
2019-04-22 10:05:07 +0200

    Subida del código

---
+++ Practica6/src/practica6/Coche.java
@@ -0,0 +1,40 @@
+package practica6;
+
+public class Coche {
+	String matricula;
+	String atrib;
+	String modelo;
+	String fabricante;
+
+	public Coche() {
+		matricula = "";
+		atrib = "";
+		modelo = "";
+		fabricante = "";
+	}
+
+	public Coche(String m, String c, String mo, String f) {
+		matricula = m;
+		atrib = c;
+		modelo = mo;
+		fabricante = f;
+	}
+
+	public String metodo1() {
+		String resultado = "";
+		if (atrib == "Gasolina") {
+			resultado += "El coche con matricula " + matricula + " ";
+			resultado += metodoA(modelo, fabricante);
+		} else if (atrib == "Diesel") {
+			resultado += "El coche con matricula " + matricula + " ";
+			resultado += metodoB(modelo, fabricante);
+		} else if (atrib == "Híbrido") {
+			resultado += "El coche con matricula " + matricula + " ";
+			resultado += metodoC(modelo, fabricante);
+		} else {
+			resultado += "El coche con matricula " + matricula + " ";
+			resultado += "no dispone de información";
+		}
+		return resultado;
+	}
+}
---
+++ Practica6/src/practica6/Moto.java
@@ -0,0 +1,32 @@
+package practica6;
+
+public class Moto {
+	String matricula;
+	int cilindrada;
+
+	public Moto() {
+		matricula = "";
+		cilindrada = 0;
+	}
+
+	public Moto(String m, int c) {
+		matricula = m;
+		cilindrada = c;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setMatricula(String m) {
+		matricula = m;
+	}
+
+	public void setCilindrada(int c) {
+		cilindrada = c;
+	}
+}
